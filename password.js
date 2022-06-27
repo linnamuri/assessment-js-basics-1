4700d85ad775d155fb5818581bae46860de03393
linnamuri <lakshmi.innamuri@gmail.com>
2022-06-27 09:37:14 +0530

    Part3-Password Validator-password.js

--- password.js
+++ password.js
@@ -1,21 +1,42 @@
 
-let password = "assignment1"
-if(password.length>=10)
-{
-    let passwordArray = password.split('')
-    console.log(passwordArray)
-for (i=0;i<passwordArray.length;i++)
-{
-    if(65<=passwordArray.charCodeAt[i]&&passwordArray.charCodeAt[i]<=122)
-    {
-console.log("found a character and looking for a number")
-//passwordArray.isFinite[i]=== true
-//console.log("Found a number")
-console.log("pasword is set successfully")
-    }
-}
-}
-else
-console.log("You failed to create a proper password. Atleast 10 characters are necessary to create a successful password.")
 
 
+    let passwordArray = "assignment1"
+
+    if (passwordArray === "password"||passwordArray === "letmein")
+        console.log("Not a valid password")
+    else if (passwordArray.indexOf(" ")>=0)
+        console.log("Password should not have spaces")
+    else if(passwordArray.length>=10 && passwordArray.length<=20)
+        {
+            var foundAplhabet=0;
+            var foundNumber=0;
+            for (i=0;i<passwordArray.length;i++)
+                {
+                    if(foundAplhabet===0)
+                        {
+                            if((passwordArray.charCodeAt(i)>=65 && passwordArray.charCodeAt(i)<=90) || (passwordArray.charCodeAt(i)>=97 && passwordArray.charCodeAt(i)<=122))
+                                {
+                                    foundAplhabet=1;
+                                }
+                        }
+                    if(isFinite(passwordArray[i])===true && foundNumber===0)
+                        {
+                            foundNumber=1;
+                        }
+                }
+            if(foundAplhabet===1 && foundNumber===1)
+            {
+                console.log("Password is set successfully")
+            }
+            else
+            {
+                console.log("Password does not meet criteria")
+            }
+   
+        }
+    else 
+        console.log("You failed to create a proper password. Password length should be between 10-20 characters.")
+
+
+  
